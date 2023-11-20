ba056fe72d4012421743d2d767a1c976f91c1c31
Edison Garcia <egachi@live.com>
2023-11-19 18:30:07 -0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import logo from './assets/1.png'
+import logo2 from './assets/2.png'
+import logo3 from './assets/3.png'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="" />
+      <img src={logo2} alt="" />
+      <img src={logo3} alt="" />
+    </div>
+  );
+}
+export default App;
