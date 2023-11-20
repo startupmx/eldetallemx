a4663b7004c717acb55630177ff09fad99945789
Edison Garcia <egachi@live.com>
2023-11-19 19:11:12 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import logo3 from './assets/3.png'
 function App() {
   return (
     <div className="App">
-      <img src={logo} alt="" />
-      <img src={logo2} alt="" />
-      <img src={logo3} alt="" />
+      <img style={{width:'100%', height:'100%'}} src={logo} alt="" />
+      <img style={{width:'100%', height:'100%'}} src={logo2} alt="" />
+      <img style={{width:'100%', height:'100%'}} src={logo3} alt="" />
     </div>
   );
 }
