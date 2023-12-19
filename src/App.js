0a07c93f0cbc9e31192f278fc5e2f638a166d85f
Edison Garcia <egachi@live.com>
2023-12-19 16:31:20 -0600

    Updating

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './assets/1.jpg'
 import logo2 from './assets/2.jpg'
 import logo3 from './assets/3.jpg'
 import logo4 from './assets/4.jpg'
+import logo5 from './assets/5.jpg'
 
 //App
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <img style={{width:'100%', height:'100%'}} src={logo2} alt="" />
       <img style={{width:'100%', height:'100%'}} src={logo3} alt="" />
       <img style={{width:'100%', height:'100%'}} src={logo4} alt="" />
+      <img style={{width:'100%', height:'100%'}} src={logo5} alt="" />
     </div>
   );
 }
