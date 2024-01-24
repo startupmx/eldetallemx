f5048f9df7f5ad9caacacc87ae3c1f71a2a6a3f8
Edison Garcia <egachi@live.com>
2024-01-23 18:20:12 -0600

    Updating

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import logo from './assets/1.jpg'
 import logo2 from './assets/2.jpg'
-
+import logo3 from './assets/3.jpg'
 //App
 function App() {
   return (
     <div className="App">
       <img style={{width:'100%', height:'100%'}} src={logo} alt="" />
       <img style={{width:'100%', height:'100%'}} src={logo2} alt="" />
+      <img style={{width:'100%', height:'100%'}} src={logo3} alt="" />
     </div>
   );
 }
